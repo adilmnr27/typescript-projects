dd9afb277faef6f783b4a518982821348271664e
adilmnr27 <adilmnr27@gmail.com>
2020-08-12 13:50:57 +0530

    Created the interface for TODO to map response data

---
+++ Basic/index.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+//defining the type of response data
+interface TODO {
+  title: string;
+  id: number;
+  completed: boolean;
+}
+
+axios.get(url).then((res) => {
+  const { id, title, completed } = res.data as TODO;
+  console.log(`The is ${id}.
+  The title is ${title}
+  The status is ${completed}`);
+});
